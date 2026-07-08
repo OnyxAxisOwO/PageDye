2f5fd1773c095e44d0aba72bfacb49d5de40f57a
OnyxAxis <onyxaxis@snesite.onmicrosoft.com>
2026-07-08 14:08:52 +0800

    chore: release version 0.7.10

--- site/pagedye-widget.js
+++ site/pagedye-widget.js
@@ -17,7 +17,7 @@
 (function () {
   'use strict';
 
-  const VERSION = '0.7.9';
+  const VERSION = '0.7.10';
   const domain = window.location.hostname;
   const STORAGE_KEY = 'pagedye-embed:' + domain;
   const GLOBAL_KEY = 'pagedye-embed:global-ui';
